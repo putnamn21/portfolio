6a7086f4660e684383ca24aac38e85461e37e573
putnamn21 <nathanputnam2@gmail.com>
2018-02-22 16:09:04 -0600

    got hello from react working

---
+++ resources/assets/js/containers/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App container">
+    <h1>Hello From React</h1>
+  </div>
+);
+
+export default App;
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            Hello Laravel
-        </div>
-    </div>
+<div id="app">
+
 </div>
 @endsection
